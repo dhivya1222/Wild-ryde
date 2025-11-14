9e0edda9a499d9ac0131a66f05d64641fb8d8d16
dhivya1222 <dhivyanandhagopal12@gmail.com>
2025-11-14 11:40:24 +0530

    Update AWS Cognito user pool configuration

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_y578h0eJS',
-        userPoolClientId: 'T2qduv81mEjfnsob4pgmgpbf',
+        userPoolId: 'ap-south-1_SXWUs0RCe',
+        userPoolClientId: '4m0qiu26g98mfam59qgd5f15uq',
         region: 'ap-south-1'
     },
     api: {
@@ -11,6 +11,7 @@ window._config = {
         clientId: '984566112203-ap9e58gecioe9csom4mej6vlae4hjtj3.apps.googleusercontent.com' // <-- Add this
     }
 };
+
 
 
 
